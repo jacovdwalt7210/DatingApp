f0b4a7d0e7b98a8329a87bf11c955ba23faa2cde
jacovdwalt7210 <jacovdwalt@outlook.com>
2020-12-15 12:51:00 +0200

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -21,7 +21,7 @@ namespace API
         private readonly IConfiguration _config;
         public Startup(IConfiguration config)
         {
-        _config = config;
+            _config = config;
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -33,6 +33,7 @@ namespace API
         });
 
         services.AddControllers();
+        services.AddCors();
         services.AddSwaggerGen(c =>
         {
             c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -42,23 +43,26 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-        if (env.IsDevelopment())
-        {
-            app.UseDeveloperExceptionPage();
-            app.UseSwagger();
-            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
-        }
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+            }
 
-        app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
 
-        app.UseRouting();
+            app.UseRouting();
+            app.UseCors(p => p.AllowAnyHeader()
+                              .AllowAnyMethod()
+                              .WithOrigins("https://localhost:4200"));
 
-        app.UseAuthorization();
+            app.UseAuthorization();
 
-        app.UseEndpoints(endpoints =>
-        {
-            endpoints.MapControllers();
-        });
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
