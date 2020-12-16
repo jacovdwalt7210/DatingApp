785b1d158fcd1caba0a371e6d1794858f7607b4c
jacovdwalt7210 <jacovdwalt@outlook.com>
2020-12-16 20:38:08 +0200

    End Of Section 4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -2,11 +2,15 @@ using System.ComponentModel.DataAnnotations;
 
 namespace API.Entities
 {
-    public class AppUser
-    {
-        public int Id { get; set; }
-        
-        [MaxLength(100)]
-        public string UserName { get; set; }
-    }
+  public class AppUser
+  {
+    public int Id { get; set; }
+
+    [MaxLength(100)]
+    public string UserName { get; set; }
+
+    public byte[] PasswordHash { get; set; }
+
+    public byte[] PasswordSalt { get; set; }
+  }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -9,20 +9,20 @@ using Microsoft.Extensions.Logging;
 
 namespace API
 {
-    public class Program
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) 
-        {
-            return Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-        }
+      CreateHostBuilder(args).Build().Run();
     }
+
+    public static IHostBuilder CreateHostBuilder(string[] args)
+    {
+      return Host.CreateDefaultBuilder(args)
+          .ConfigureWebHostDefaults(webBuilder =>
+          {
+            webBuilder.UseStartup<Startup>();
+          });
+    }
+  }
 }
